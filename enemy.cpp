7bec591f2d458766a14db544827e613504e52d97
Sidhanth Kafley <skafley@uvm.edu>
2020-04-28 00:32:02 -0400

    Add Shapes class

---
+++ enemy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sidhanth Kafley on 4/27/20.
+//
+
+#include "enemy.h"
---
+++ enemy.h
@@ -0,0 +1,14 @@
+//
+// Created by Sidhanth Kafley on 4/27/20.
+//
+
+#ifndef SH_SK_FINAL_PROJECT_GRAPHICS_ENEMY_H
+#define SH_SK_FINAL_PROJECT_GRAPHICS_ENEMY_H
+
+
+class enemy {
+
+};
+
+
+#endif //SH_SK_FINAL_PROJECT_GRAPHICS_ENEMY_H
